3196ca7330424892e3d35221a9ff041f688a5fa4
zhaoying du <dcd0413@gmail.com>
2019-02-28 18:51:46 -0500

    add db

--- R/ibcfRec.R
+++ R/ibcfRec.R
@@ -6,7 +6,7 @@ connectDB <- function(){
   password<-'zhaoyingdu'
   driver<-dbDriver('PostgreSQL')
   connection<-dbConnect(drv = driver, dbname='temp',
-      host='127.0.0.1', port = 5109,
+      host='172.17.86.131', port = 5109,
       user=username, password = password
     )
   #print(dbGetInfo(connection))
@@ -18,18 +18,18 @@ connectDB <- function(){
 ibcfRec = function(userID){
   # todo: 
   connection = connectDB()
-  dbGetInfo(connection)
+  print(dbGetInfo(connection))
   data = dbReadTable(connection, 
     'uirtable')
   
   usefulData <- data.frame(user = data$uid, item = data$itemid, rating = data$rating)
   ratingMatrix = as(data, 'realRatingMatrix')
   userVector = ratingMatrix[userID,]
-  if(!file.exists('./recModel.rds')){
+  if(!file.exists('recModel.rds')){
     recModel <- Recommender(ratingMatrix[1:500], method='IBCF')
-    saveRDS(recModel, './recModel.rds')
+    saveRDS(recModel, 'recModel.rds')
   }
-  recModel<-readRDS('./recModel.rds')
+  recModel<-readRDS('recModel.rds')
   recommendation <- predict(recModel, userVector, n=10)
   as(recommendation, "list")  
 }
---
+++ R/script.R
@@ -0,0 +1,1 @@
+print(readRDS('hi.rds'))
