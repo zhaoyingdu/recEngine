ed18fb99647ce1540c96c05ae57ae9ebcea9c649
zhaoying du <dcd0413@gmail.com>
2019-02-28 00:59:13 -0500

    'filepath'

--- R/ibcfRec.R
+++ R/ibcfRec.R
@@ -4,11 +4,11 @@ ibcfRec = function(userID){
   usefulData <- data.frame(user = data$uid, item = data$itemId, rating = data$rating)
   ratingMatrix = as(usefulData, 'realRatingMatrix')
   userVector = ratingMatrix[userID,]
-  if(!file.exists('recModel.rds')){
+  if(!file.exists('./recModel.rds')){
     recModel <- Recommender(ratingMatrix[1:500], method='IBCF')
-    saveRDS(recModel, 'recModel.rds')
+    saveRDS(recModel, './recModel.rds')
   }
-  recModel<-readRDS('recModel.rds')
+  recModel<-readRDS('./recModel.rds')
   recommendation <- predict(recModel, userVector, n=10)
   as(recommendation, "list")  
 }
